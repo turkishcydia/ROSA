b2b31a0dd5074e5458c01549bcf98b8ed3fe9d85
__UwU__ <82547225+turkishcydia@users.noreply.github.com>
2023-02-18 00:49:22 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+git reset --hard
+chmod +x *
 git pull > tmp
+chmod +x *
 
 while read -r row; do
 echo "$row"
@@ -9,15 +12,15 @@ if [ $row="Already up to date." ]
 then
 clear
 echo "PRoot-distro ve Rosa zaten güncel."
-else
+fi
 echo "[*] Gerekli yamalar yükleniyor..."
 git pull
 sleep 2
-sh install.sh
+./install.sh
 clear
 echo "[*] Eski veriler yedekleniyor. Eğer yedeklenmez ise yeni sistem kurulduğu zaman bütün verileriniz kayıp olur."
 sleep 2
-proot-distro backup rosa --output rosa_old.tar.xz
+proot-distro backup rosa --output $HOME/rosa_old.tar.xz
 clear
 echo "[*] Eski sistem siliniyor."
 sleep 2
@@ -28,11 +31,10 @@ sleep 2
 proot-distro install rosa
 clear
 echo "[*] Eski verileriniz yeni sisteme aktarılıyor."
-tar -xf --skip-old-files rosa_old.tar.xz -C $HOME/../usr/var/lib/proot-distro/installed-rootfs/rosa/
+tar -xf --skip-old-files $HOME/rosa_old.tar.xz -C $HOME/../usr/var/lib/proot-distro/installed-rootfs/rosa/
 clear
 echo "Önbellek siliniyor..."
 rm tmp
-rm rosa_old.tar.xz
+rm $HOME/rosa_old.tar.xz
 clear
-echo "PRoot ve Rosa başarıyla güncellendi! Şimdi kullanabilirsiniz."
-fi
+echo "PRoot ve Rosa başarıyla güncellendi! Şimdi kullanabilirsiniz."qqqqaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

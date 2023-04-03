e34b0fdbb8e250d5d3bd6393df232a9b152de994
__UwU__ <82547225+turkishcydia@users.noreply.github.com>
2023-04-03 22:36:34 +0300

    Update rosa.sh

--- distro-plugins/rosa.sh
+++ distro-plugins/rosa.sh
@@ -2,4 +2,4 @@ DISTRO_NAME="Rosa"
 DISTRO_COMMENT="v1.0"
 
 TARBALL_URL['aarch64']="https://github.com/turkishcydia/ROSA/releases/download/rosafs/rosa-aarch64.tar.gz"
-TARBALL_SHA256['aarch64']="a26a31b388fb3413d269e4eff4c71df6d421530dee209b18c48e0c7d996546c3"
+TARBALL_SHA256['aarch64']="12a38c1bffbd9c34ddbff48d1dcde25325dd6747eade1a74ee5043468d0c5b55"

b74170164d5ef0224c4264ae7bf4332407e77adc
__UwU__ <82547225+turkishcydia@users.noreply.github.com>
2023-04-04 18:22:21 +0300

    Delete distro-plugins directory

--- distro-plugins/rosa.sh
+++
@@ -1,5 +0,0 @@
-DISTRO_NAME="Rosa"
-DISTRO_COMMENT="v1.0"
-
-TARBALL_URL['aarch64']="https://github.com/turkishcydia/ROSA/releases/download/rosafs/rosa-aarch64.tar.gz"
-TARBALL_SHA256['aarch64']="010ac9c5a97c5b984ae4e94639e6e6f1b4a7cac08627862fbdffd151fcc67a38"
